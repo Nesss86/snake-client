e3c85515752492ba34a59579dc808fe0abeae1c0
Nesss86 <Nessymonster86@gmail.com>
2024-11-20 19:50:19 -0500

    minor updates

--- client.js
+++ client.js
@@ -8,7 +8,13 @@ const connect = function () {
 
   conn.setEncoding("utf8");
 
- 
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server");
+
+  const name = "ABC"; // Change this to any three-letter string
+    conn.write(`Name: ${name}`);
+  });
+
   conn.on("data", (data) => {
     console.log("Server says:", data);
   });
